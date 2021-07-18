825869bbd7cfe6f49e5e39cc6d5af3300937b4e7
Burke Holland <burkeholland@gmail.com>
2021-07-18 21:49:04 +0000

    document

--- api/GetProducts/index.js
+++ api/GetProducts/index.js
@@ -1,5 +1,7 @@
 const products = require("../data/products.json");
 
+// get more products
+
 module.exports = async function (context, req) {
     context.res = {
         // status: 200, /* Defaults to 200 */
